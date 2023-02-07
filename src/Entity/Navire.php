60979650686b53dd5e3208f249d5e2cafa966552
Rayane KADRI <kadrirayane@gmail.com>
2023-02-07 13:36:42 +0100

    Création de l'entité Navire et de la migration

---
+++ src/Entity/Navire.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NavireRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NavireRepository::class)]
+class Navire
+{
+    #[Assert\Unique(fields:['imo','mmsi','indicatifAppel'])]
+            
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 7)]
+    #[Assert\Regex('[1-9][0-9]{6}', message : 'le numéro IMO doit être unique et composé de 7 chiffres sans commencer par 0')]
+    private ?string $imo = null;
+
+    #[ORM\Column(length: 100)]
+    #[Assert\Regex('[0-9a-zA-Z]{3,}', message : 'le nom du navire doit contenir 3 caractères alphanumériques au minimum')]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 9)]
+    #[Assert\Regex('[1-9][0-9]{6}', message : 'le numéro MMSI doit être unique et composé de 9 chiffres sans commencer par 0')]
+    private ?string $mmsi = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $indicatifAppel = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $eta = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImo(): ?string
+    {
+        return $this->imo;
+    }
+
+    public function setImo(string $imo): self
+    {
+        $this->imo = $imo;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMmsi(): ?string
+    {
+        return $this->mmsi;
+    }
+
+    public function setMmsi(string $mmsi): self
+    {
+        $this->mmsi = $mmsi;
+
+        return $this;
+    }
+
+    public function getIndicatifAppel(): ?string
+    {
+        return $this->indicatifAppel;
+    }
+
+    public function setIndicatifAppel(string $indicatifAppel): self
+    {
+        $this->indicatifAppel = $indicatifAppel;
+
+        return $this;
+    }
+
+    public function getEta(): ?\DateTimeInterface
+    {
+        return $this->eta;
+    }
+
+    public function setEta(?\DateTimeInterface $eta): self
+    {
+        $this->eta = $eta;
+
+        return $this;
+    }
+}
